aab7b625b1c9f6e1faf369a3740078475cfca0ac
miyatarutyyy <study.miyata026@gmail.com>
2026-01-27 14:15:58 +0900

    fix: fix Astro base/site for root deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://geeken-iniad.github.io",
-  base: "/web-site/",
+  site: "https://geeken-iniad.org",
   output: "static",
 
   env: {
